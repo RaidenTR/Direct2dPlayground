7d1e5582125bd450efb680df9fcc249b0a4b4b4e
Borys Sadovnykov <borys.sadovnykov@dct.ua>
2021-01-30 17:09:37 +0200

    initial

---
+++ Direct2dPlayground/Direct2dPlayground.h
@@ -0,0 +1,3 @@
+#pragma once
+#include "ThrowIfFailed.h"
+#include "resource.h"
---
+++ Direct2dPlayground/ThrowIfFailed.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <Windows.h>
+#include <exception>
+
+namespace DX
+{
+    inline void ThrowIfFailed(HRESULT hr)
+    {
+        if (FAILED(hr))
+        {
+            // Set a breakpoint on this line to catch DirectX API errors
+            throw std::exception();
+        }
+    }
+}
